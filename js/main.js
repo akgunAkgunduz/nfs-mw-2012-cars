e566eda8c1bc5680ab684883edbe6fb71955d9e7
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-08-28 12:30:56 +0300

    Change structure of cars variable and update car list

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
-
-for (car in cars) {
+cars.forEach(car => {
   let newTr = document.createElement('tr')
-  let newTd = document.createElement('td')
-  
-  newTd.textContent = cars[car].make + ' ' + cars[car].model
-  
-  newTr.appendChild(newTd)
+  let keys = Object.keys(car)
+  keys.forEach((property, i) => {
+    if ((i !== 1) && (i !== 2) && (i !== 4)) {
+      let newTd = document.createElement('td')
+      newTd.textContent = car[property]
+      newTr.appendChild(newTd)
+    }
+  })
   carTable.querySelector('tbody').appendChild(newTr)
-}
+})
