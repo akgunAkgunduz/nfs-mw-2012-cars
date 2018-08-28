c31fc49b1b5e7a242049b91dd175421e106fa6ef
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-08-28 13:36:45 +0300

    Add jQuery, Bootstrap and DataTables

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-cars.forEach(car => {
-  let newTr = document.createElement('tr')
-  let keys = Object.keys(car)
-  keys.forEach((property, i) => {
-    if ((i !== 1) && (i !== 2) && (i !== 4)) {
-      let newTd = document.createElement('td')
-      newTd.textContent = car[property]
-      newTr.appendChild(newTd)
-    }
+$(document).ready( function () {  
+  cars.forEach(car => {
+    let newTr = document.createElement('tr')
+    let keys = Object.keys(car)
+    keys.forEach((property, i) => {
+      if ((i !== 1) && (i !== 2) && (i !== 4)) {
+        let newTd = document.createElement('td')
+        newTd.textContent = car[property]
+        newTr.appendChild(newTd)
+      }
+    })
+    carsTable.querySelector('tbody').appendChild(newTr)
   })
-  carTable.querySelector('tbody').appendChild(newTr)
+
+  $('#carsTable').DataTable()
 })
