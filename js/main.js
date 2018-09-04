4f3ebc16602cf56cfe7ea2397e98a5bec33182c2
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-09-04 21:09:19 +0300

    Add query object creation functionality

--- js/main.js
+++ js/main.js
@@ -14,10 +14,26 @@ $(document).ready( function () {
     ]
   })
 
+  let query = {
+    game: [],
+    class: []
+  }
+
   test.addEventListener('click', () => {
     const result = cars.filter(car => car.game == 'stock')
     carsTable.clear()
     carsTable.rows.add(result)
     carsTable.draw()
+  })
+
+  $('.form-check-input').on('click', function(e) {
+    if (e.target.checked) {
+      query[e.target.dataset.key].push(e.target.value)
+    } else {
+      const index = query[e.target.dataset.key].indexOf(e.target.value)
+      query[e.target.dataset.key].splice(index, 1)
+    }
+    console.log(JSON.stringify(query, null, '\t'))
+    
   })
 })
