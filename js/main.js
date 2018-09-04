7d5b7d91941349830bf8026e0ff3a7d623a88c40
Akgün Akgündüz <akgun.akgunduz@gmail.com>
2018-09-05 00:31:55 +0300

    Add filtering functionality and remove test button

--- js/main.js
+++ js/main.js
@@ -15,16 +15,9 @@ $(document).ready( function () {
   })
 
   let query = {
-    game: [],
-    class: []
+    game: ['stock', 'speed', 'terminal', 'hero', 'movie', 'multi'],
+    class: ['Exotic', 'Sports', 'Race', 'Muscle', 'Grand Tourer', 'Everyday', 'SUV']
   }
-
-  test.addEventListener('click', () => {
-    const result = cars.filter(car => car.game == 'stock')
-    carsTable.clear()
-    carsTable.rows.add(result)
-    carsTable.draw()
-  })
 
   $('.form-check-input').on('click', function(e) {
     if (e.target.checked) {
@@ -33,7 +26,61 @@ $(document).ready( function () {
       const index = query[e.target.dataset.key].indexOf(e.target.value)
       query[e.target.dataset.key].splice(index, 1)
     }
-    console.log(JSON.stringify(query, null, '\t'))
+    // console.log(JSON.stringify(query, null, '\t'))
     
+    if (query['game'].length > 0 && query['class'].length > 0) {
+      filter.disabled = false
+    } else {
+      filter.disabled = true
+    }    
   })
+
+  $('#filter').on('click', function() {
+    filterByCriteria(query, cars, carsTable)
+  })
+
+  $('.check-all').on('click', function() {
+    const checkboxes = $(this).closest('.card').find('input[type="checkbox"]')
+    checkboxes.each(i => {
+      if(checkboxes[i].checked == false) {
+        checkboxes[i].click()
+      }
+    })
+  })
+
+  $('.clear-all').on('click', function() {
+    const checkboxes = $(this).closest('.card').find('input[type="checkbox"]')
+    checkboxes.each(i => {
+      if(checkboxes[i].checked == true) {
+        checkboxes[i].click()
+      }
+    })
+  })  
 })
+
+function filterByCriteria(criteria, items, table) {
+  const criteriaKeys = Object.keys(criteria)  
+  let conditionsMatched = 0  
+  let result = []
+  
+  items.forEach(item => {
+    conditionsMatched = 0
+
+    criteriaKeys.forEach(criteriaKey => {
+      if (criteria[criteriaKey].indexOf(item[criteriaKey]) > -1) {
+        conditionsMatched++
+
+        if (conditionsMatched === criteriaKeys.length) {
+          result.push(item)
+          conditionsMatched = 0
+        }
+      } else {
+        conditionsMatched = 0
+      }
+    })
+  })
+
+  table.clear()
+  table.rows.add(result)
+  table.draw()  
+}
